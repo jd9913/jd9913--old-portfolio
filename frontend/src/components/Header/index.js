3336e86722c8652f7dda4fba754879b1bc6bd848
Jennifer Schlierman <jd9913@gmail.com>
2021-02-15 14:46:05 -0700

    styling header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -8,7 +8,8 @@ const Header = () => {
 				<Navbar.Brand href='/'>Jennifer Davey Schlierman</Navbar.Brand>
 				<Navbar.Toggle aria-controls='basic-navbar-nav' />
 				<Navbar.Collapse id='basic-navbar-nav'>
-					<Nav className='mr-auto'>
+					<Nav className='ml-auto'>
+						<Nav.Link href='/about'>About Me</Nav.Link>
 						<Nav.Link href='/projects'>Projects</Nav.Link>
 						<Nav.Link href='/resume'>Resume</Nav.Link>
 						<Nav.Link href='/contact'>Contact</Nav.Link>
