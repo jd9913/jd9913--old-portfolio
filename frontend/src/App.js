2a91cb2b4ad9631463ceb7d83802f70259397a64
Jennifer Schlierman <jd9913@gmail.com>
2021-02-15 15:12:51 -0700

    creating pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,29 @@
 import logo from "./logo.svg";
-import About from "./components/About";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
+import ContactPage from "./Pages/ContactPage";
+import ProjectsPage from "./Pages/ProjectsPage";
+import ResumePage from "./Pages/ResumePage";
 
 function App() {
 	return (
-		<main>
+		<Router>
 			<Header />
-			<div className='App'>
-				<header className='App-header'>
-					<img src={logo} className='App-logo' alt='logo' />
-					<About></About>
-				</header>
-			</div>
-		</main>
+			<main className='py-3'>
+				<Container>
+					<Route path='/' component={HomePage} exact />
+					<Route path='/about' component={AboutPage} />
+					<Route path='/contact' component={ContactPage} />
+					<Route path='/projects' component={ProjectsPage} />
+					<Route path='/resume' component={ResumePage} />
+				</Container>
+			</main>
+			<Footer />
+		</Router>
 	);
 }
 
